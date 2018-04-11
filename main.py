82622542289ee73aa035eb410ab713f6c4f78a61
Lucas Cima <lucas.cima92@gmail.com>
2018-04-10 20:57:12 -0500

    figuring it out

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blogging@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(300))
+
+    def __init__(self, name):
+        self.name = name
+        self.completed = False
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        blog_name = request.form['blog']
+        new_blog = Blog(blog_name)
+        db.session.add(new_blog)
+        db.session.commit()
+
+    blog = Blog.query.all()
+    return render_template("blogs.html", title="Get It Done!", blog=blog)
+
+
+if __name__ == '__main__':
+    app.run()
