adf0ac7bd7836fccc72bf4e86414e59ef92f502e
Lucas Cima <lucas.cima92@gmail.com>
2018-04-15 17:33:07 -0500

    one step closer

--- main.py
+++ main.py
@@ -29,17 +29,32 @@ def display_blog():
     blogs = Blog.query.all()
     return render_template("index.html", blogs=blogs)
 
-
 @app.route('/new_post', methods=['POST', 'GET'])
 def new_post():
+    if request.method == 'GET':
+        return render_template('new_post.html')
     if request.method == 'POST':
         blog_title = request.form['title']
-        blog_name = request.form['blog']
-        new_blog = Blog(blog_title, blog_name)
-        db.session.add(new_blog)
-        db.session.commit()
-    
-    return render_template('new_post.html')
+        blog_body = request.form['blog']
+        title_error = ''
+        body_error = ''
+        
+        if len(blog_title) == 0:
+            title_error = 'Please fill out a title'
+        
+        if len(blog_body) == 0:
+            body_error = 'Please fill in a blog'
+        
+        if not title_error and not body_error:
+            new_blog = Blog(blog_title, blog_body)
+            db.session.add(new_blog)
+            db.session.commit()
+            return redirect('/blog')
+        
+        else:
+            return render_template('new_post.html', title_error=title_error, body_error=body_error)
+
+
 
 if __name__ == '__main__':
     app.run()
